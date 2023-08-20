f1e4882387225a6f23794abcf26581486e7d98e5
kamilzyczynski <kamil.zyczynski@gmail.com>
2023-08-20 15:29:38 +0200

    Initial commit

---
+++ src/main/java/com/example/aplikacjabukmacherska/AplikacjaBukmacherskaApplication.java
@@ -0,0 +1,13 @@
+package com.example.aplikacjabukmacherska;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikacjaBukmacherskaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AplikacjaBukmacherskaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aplikacjabukmacherska/AplikacjaBukmacherskaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aplikacjabukmacherska;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikacjaBukmacherskaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
